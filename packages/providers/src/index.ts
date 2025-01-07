6c24167a1e48eed367a51d1d9c2a9ddc731085aa
sonnv <sonnv1912@gmail.com>
2025-01-07 14:27:11 +0700

    feat: add use client

--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -1,11 +1,4 @@
-// 'use client';
+'use client';
 
-// import dynamic from 'next/dynamic';
-
-// const ReduxProvider = dynamic(() => import('./redux'), {
-//    ssr: false,
-// });
-
-// export { ReduxProvider };
 export * from './query';
 export * from './redux';
